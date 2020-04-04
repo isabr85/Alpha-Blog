572247523e26a4cc9766c58892e916dd60235ecd
Israel Abramov <israel.abramov@flywire.com>
2020-04-04 04:02:18 +0300

    End of CRUD actions

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,38 +1,51 @@
+# frozen_string_literal: true
+
 class ArticlesController < ApplicationController
+  before_action :set_article, only: %i[show edit update destroy]
 
-    def show
-        @article = Article.find(params[:id])
-    end
+  def show; end
 
-    def index
-        @articles = Article.all
-    end
+  def index
+    @articles = Article.all
+  end
 
-    def new
-        @article = Article.new
-    end
+  def new
+    @article = Article.new
+  end
 
-    def edit
-        @article = Article.find(params[:id])
-    end
+  def edit; end
 
-    def create
-        @article = Article.new(params.require(:article).permit(:title, :description))
-        if @article.save
-            flash[:notice] = "Article was created successfully."
-            redirect_to article_path(@article)
-        else
-            render 'new'
-        end
+  def create
+    @article = Article.new(article_params)
+    if @article.save
+      flash[:notice] = 'Article was created successfully.'
+      redirect_to article_path(@article)
+    else
+      render 'new'
     end
+  end
 
-    def update
-        @article = Article.find(params[:id])
-        if @article.update(params.require(:article).permit(:title, :description))
-            flash[:notice] = "Article was updated successfully."
-            redirect_to article_path(@article)
-        else
-            render 'edit'
-        end
+  def update
+    if @article.update(article_params)
+      flash[:notice] = 'Article was updated successfully.'
+      redirect_to article_path(@article)
+    else
+      render 'edit'
     end
+  end
+
+  def destroy
+    @article.destroy
+    redirect_to articles_path
+  end
+
+  private
+
+  def set_article
+    @article = Article.find(params[:id])
+  end
+
+  def article_params
+    params.require(:article).permit(:title, :description)
+  end
 end
